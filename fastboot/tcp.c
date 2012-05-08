526561f0736c93de00d0caab41f59f05b9b82ddd
Matt Gumbel <matthew.k.gumbel@intel.com>
2012-05-08 11:03:01 -0700

    [PORT FROM MAIN] Fastboot over TCP transport
    BZ: 110558, 112257, 112911, 113861, 112829
    
    That is a squash of the patches created for above BZ.
    For devices which do not have USB, we can run fastboot over the
    network. This requires corresponding TCP support in droidboot.
    
    Change-Id: I7a9b035bcfb72b23a85db0b00890673a1af9548a
    Orig-Change-Id: Ic93a88a3a5dfe21e0b2788add807afbcddbf49cd
    Category: feature differentiation
    Domain: PUPDR-Recovery
    Origin: internal
    Upstream-Candidate: no
    Conflicts:
    	fastboot/fastboot.c => made adaptation to new PDK code
    Orig-Change-Id: I23037879c1d7e9a59012736c6bb6451005f86629
    Conflicts:
    	fastboot/usb_windows.c (Google changed a timeout value)
    Orig-Change-Id: Id293a30acf986d39742897e48047ed6a628119a0
    Orig-Change-Id: I59a67a861af13cdf9fe44b8828338c33ce3249da
    Orig-Change-Id: I3ad172356e25af4ae866f891c78a9adebad796f0

---
+++ fastboot/tcp.c
@@ -0,0 +1,297 @@
+/*
+ * Copyright (C) 2008 The Android Open Source Project
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the 
+ *    distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED 
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <errno.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+
+#ifdef _WIN32
+#include <winsock2.h>
+typedef int socklen_t;
+#define hstrerror(a) strerror(errno)
+#else
+#include <sys/socket.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <fcntl.h>
+extern int h_errno;
+#endif
+
+#include "tcp.h"
+#include "fastboot.h"
+
+#define FSTBOOT_PORT 1234
+#define FSTBOOT_DFL_ADDR "192.168.42.1"
+
+int tcp_write(void *userdata, const void *_data, int len)
+{
+    int len_tmp = len;
+    int n;
+    const char *_data_tmp = _data;
+    tcp_handle *h = userdata;
+    while (len_tmp > 0) {
+        n = send(h->sockfd, _data_tmp, len_tmp, 0);
+        if (n <= 0) {
+            switch(errno) {
+            case EAGAIN: case EINTR: continue;
+            default:
+                fprintf(stderr, "ERROR: Failed to send to network: %s\n",
+                        strerror(errno));
+                exit(1);
+            }
+        }
+        len_tmp -= n;
+        _data_tmp += n;
+    }
+    return len;
+}
+
+int tcp_read(void *userdata, void *_data, int len)
+{
+    int n, count;
+    tcp_handle *h = userdata;
+    count = 0;
+    while (len > 0) {
+        // This xfer chunking is to mirror usb_read() implementation:
+        int xfer = (len > 16*1024) ? 16*1024 : len;
+        n = recv(h->sockfd, _data, xfer, 0);
+        if (n == 0) {
+            fprintf(stderr, "ERROR: Failed to read network: "
+                    "Unexpected end of file.");
+            exit(1);
+        } else if (n < 0) {
+            switch(errno) {
+            case EAGAIN: case EINTR: continue;
+            default:
+                fprintf(stderr, "ERROR: Failed to read network: %s",
+                        strerror(errno));
+                exit(1);
+            }
+        }
+        count += n;
+        len -= len;
+        _data += n;
+
+        // Replicate a bug from usb_read():
+        if (n < xfer)
+            break;
+    }
+    return count;
+}
+
+int tcp_close(void *userdata)
+{
+    tcp_handle *h = userdata;
+    return close(h->sockfd);
+}
+
+#ifdef _WIN32
+void exit_os(void) {
+    WSACleanup();
+}
+
+void init_os(void) {
+    WSADATA wsaData;
+    int iResult;
+
+    // Initialize Winsock
+    iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (iResult != 0) {
+        printf("WSAStartup failed: %d\n", iResult);
+        exit(1);
+    }
+    atexit(exit_os);
+}
+#endif
+
+static int tcp_open_sock(char *host, struct sockaddr_in *serv_addr)
+{
+    int sockfd;
+    struct hostent *server;
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        fprintf(stderr, "ERROR: Can't open socket: %s\n", strerror(errno));
+        return sockfd;
+    }
+
+    server = gethostbyname(host);
+    if (server == NULL) {
+        fprintf(stderr, "ERROR: Can't find '%s': %s\n", host, hstrerror(h_errno));
+        return -1;
+    }
+
+    memset(serv_addr, sizeof(*serv_addr), 0);
+    serv_addr->sin_family = AF_INET;
+    memcpy(&serv_addr->sin_addr.s_addr,
+           server->h_addr,
+           server->h_length);
+    serv_addr->sin_port = htons(FSTBOOT_PORT);
+    return sockfd;
+}
+
+/*
+ * try to open a connection to 192.168.42.1:1234
+ * in less than 100ms
+ */
+void tcp_list(void)
+{
+#ifndef _WIN32
+    int sockfd;
+    struct sockaddr_in serv_addr;
+    int ret;
+    fd_set wfds;
+    struct timeval tv;
+
+    sockfd = tcp_open_sock(FSTBOOT_DFL_ADDR, &serv_addr);
+    if (sockfd < 0)
+        return;
+
+    if (fcntl(sockfd, F_SETFL, O_NONBLOCK) < 0)
+        return;
+
+    if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+        if (errno != EINPROGRESS)
+            return;
+
+        FD_ZERO(&wfds);
+        FD_SET(sockfd, &wfds);
+
+        /* Wait 100ms to connect. */
+        tv.tv_sec = 0;
+        tv.tv_usec = 100000;
+
+        ret = select(sockfd + 1, NULL, &wfds, NULL, &tv);
+        if (ret < 0)
+            return;
+        if (!FD_ISSET(sockfd, &wfds))
+            return;
+        if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+            return;
+    }
+    list_devices_callback(FSTBOOT_DFL_ADDR, NULL);
+    close(sockfd);
+#else
+    SOCKET s;
+    SOCKADDR_IN target;
+    WSADATA wsaData;
+    int iError;
+    u_long NonBlock=1;
+
+    // Initialize Winsock
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        return;
+    }
+
+   //Create a socket
+    if((s = socket(AF_INET , SOCK_STREAM , 0 )) == INVALID_SOCKET) {
+        WSACleanup();
+        return;
+    }
+
+    /* Non blocking socket */
+    NonBlock = 1;
+    if (ioctlsocket(s, FIONBIO, &NonBlock) == SOCKET_ERROR) {
+        goto clear_env;
+    }
+
+    target.sin_addr.s_addr = inet_addr(FSTBOOT_DFL_ADDR);
+    target.sin_family = AF_INET;
+    target.sin_port = htons( FSTBOOT_PORT );
+    //Connect to remote server
+    if(connect(s , (struct sockaddr *)&target , sizeof(target)) == SOCKET_ERROR) {
+        iError = WSAGetLastError();
+        //check if error was WSAEWOULDBLOCK, where we'll wait
+        if(iError == WSAEWOULDBLOCK) {
+            fd_set Write, Err;
+            TIMEVAL Timeout;
+
+            FD_ZERO(&Write);
+            FD_ZERO(&Err);
+            FD_SET(s, &Write);
+            FD_SET(s, &Err);
+            Timeout.tv_sec = 0;
+            Timeout.tv_usec = 100000;
+            iError = select(0,           //ignored
+                            NULL,        //read
+                            &Write,      //Write Check
+                            &Err,        //Error Check
+                            &Timeout);
+            if(iError == 0) {
+                goto clear_env;
+            } else {
+                if(FD_ISSET(s, &Write)) {
+                    list_devices_callback(FSTBOOT_DFL_ADDR, NULL);
+                    goto clear_env;
+                 }
+                if(FD_ISSET(s, &Err)) {
+                    goto clear_env;
+                }
+            }
+        } else {
+            goto clear_env;
+        }
+    } else {
+        //connected without waiting (will never execute)
+        list_devices_callback(FSTBOOT_DFL_ADDR, NULL);
+        goto clear_env;
+    }
+clear_env:
+    closesocket(s);
+    WSACleanup();
+
+#endif
+}
+
+tcp_handle *tcp_open(const char *host)
+{
+    int sockfd;
+    struct sockaddr_in serv_addr;
+    tcp_handle *tcp = 0;
+
+#ifdef _WIN32
+    init_os();
+#endif
+
+    sockfd = tcp_open_sock(FSTBOOT_DFL_ADDR, &serv_addr);
+    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) {
+        fprintf(stderr, "ERROR: Unable to connect to %s: %s\n",
+                host, strerror(errno));
+        exit(1);
+    }
+
+    tcp = calloc(1, sizeof(tcp_handle));
+    if (tcp != NULL) {
+        tcp->sockfd = sockfd;
+    }
+    return tcp;
+}
+
