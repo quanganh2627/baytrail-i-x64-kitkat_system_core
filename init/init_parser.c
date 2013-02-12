0ba4cf66a6623cf528126454ce06457c2cbc9e60
Sathyanarayanan Kuppuswamy <sathyanarayanan.kuppuswamy@intel.com>
2013-02-12 11:18:12 -0800

    [PORT FROM MAIN]Add modprobe-like capability and automatic uevent loading
    BZ: 87126
    
    Adds modprobe support based on uevent.This patch is ported
    from OTC android tree.
    
    354df89 Support module alias in insmod_by_dep
    4f13810 Refine return values of module loading API
    78ac9bb Improve module black lists in Android IA
    c795f24 Copy parser for black list and alias to libcutils
    f6387be Add aklog tool for kernel log
    d133f1a add modprobe-like capability and automatic ueventd loading
    
    Change-Id: I60fa83bd94cecff902aa0f5839a83eb95b280841
    Orig-Change-Id: Ia9443d96696ececddebee3a593e6492e0f2aa6c5
    Category: device enablement
    Domain: AOSP
    Origin: internal
    Upstream-Candidate: no
    Signed-off-by: Sathyanarayanan Kuppuswamy <sathyanarayanan.kuppuswamy@intel.com>
    Signed-off-by: Jeremie GARCIA <jeremie.garcia@intel.com>

--- init/init_parser.c
+++ init/init_parser.c
@@ -91,6 +91,7 @@ int lookup_keyword(const char *s)
         if (!strcmp(s, "onsole")) return K_console;
         if (!strcmp(s, "hown")) return K_chown;
         if (!strcmp(s, "hmod")) return K_chmod;
+        if (!strcmp(s, "oldboot")) return K_coldboot;
         if (!strcmp(s, "ritical")) return K_critical;
         break;
     case 'd':
@@ -132,6 +133,7 @@ int lookup_keyword(const char *s)
         break;
     case 'p':
         if (!strcmp(s, "owerctl")) return K_powerctl;
+        else if (!strcmp(s, "robemod")) return K_probemod;
     case 'r':
         if (!strcmp(s, "estart")) return K_restart;
         if (!strcmp(s, "estorecon")) return K_restorecon;
