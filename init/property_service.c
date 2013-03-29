082ec862a489d5ece6209c2576496d938f0c3635
buildslave <buildslave@buildbot.tl.intel.com>
2013-03-29 00:19:23 -0700

    Automatically revert commits from previous weekly release
    Change-Id: I9e5b1ff2bbf4b1212a643ed9c13aa5efe78a09ed
    Signed-off-by: buildslave <buildslave@buildbot.tl.intel.com>

--- init/property_service.c
+++ init/property_service.c
@@ -157,12 +157,12 @@ out:
     return -1;
 }
 
-/* (8 header words + 496 toc words) = 2016 bytes */
-/* 2048 bytes header and toc + 496 prop_infos @ 128 bytes = 65536 bytes */
+/* (8 header words + 247 toc words) = 1020 bytes */
+/* 1024 bytes header and toc + 247 prop_infos @ 128 bytes = 32640 bytes */
 
-#define PA_COUNT_MAX  496
-#define PA_INFO_START 2048
-#define PA_SIZE       65536
+#define PA_COUNT_MAX  247
+#define PA_INFO_START 1024
+#define PA_SIZE       32768
 
 static workspace pa_workspace;
 static prop_info *pa_info_array;
