6cf0b44e85fe76eaceececb0c27bb6ddf82fd128
Constantin Musca <constantin.musca@intel.com>
2013-05-28 11:47:14 +0300

    init: Add ifconfig command
    Usage: ifconfig <interface> <ip_address> <netmask>
    This command is needed for configuring an interface when using
    a minimal rootfs.
    
    Issue: ADEV-148
    Change-Id: I67d583126c8fdfbeacfda66b68f9ecb7e6677c3c
    Category: Device Enablement
    Domain: AOSP-init
    Upstream-candidate: Yes
    Signed-off-by: Constantin Musca <constantin.musca@intel.com>
    Reviewed-on: https://android.intel.com/169708
    Reviewed-by: Gumbel, Matthew K <matthew.k.gumbel@intel.com>
    Reviewed-by: Jovanovic, Radivoje <radivoje.jovanovic@intel.com>
    Reviewed-by: Boie, Andrew P <andrew.p.boie@intel.com>
    Tested-by: Boie, Andrew P <andrew.p.boie@intel.com>
    Reviewed-on: https://android.intel.com/175839
    Reviewed-by: Uyyala, Sridhar <sridhar.uyyala@intel.com>
    Tested-by: Uyyala, Sridhar <sridhar.uyyala@intel.com>

--- init/keywords.h
+++ init/keywords.h
@@ -9,6 +9,7 @@ int do_domainname(int nargs, char **args);
 int do_exec(int nargs, char **args);
 int do_export(int nargs, char **args);
 int do_hostname(int nargs, char **args);
+int do_ifconfig(int nargs, char **args);
 int do_ifup(int nargs, char **args);
 int do_insmod(int nargs, char **args);
 int do_mkdir(int nargs, char **args);
@@ -58,6 +59,7 @@ enum {
     KEYWORD(export,      COMMAND, 2, do_export)
     KEYWORD(group,       OPTION,  0, 0)
     KEYWORD(hostname,    COMMAND, 1, do_hostname)
+    KEYWORD(ifconfig,    COMMAND, 3, do_ifconfig)
     KEYWORD(ifup,        COMMAND, 1, do_ifup)
     KEYWORD(insmod,      COMMAND, 1, do_insmod)
     KEYWORD(import,      SECTION, 1, 0)
