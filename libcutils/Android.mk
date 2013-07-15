3218a84b1e43bbabb3418081cef6e40a72aaaeef
Chetan Rao <chetan.rao@intel.com>
2013-07-15 15:36:45 +0300

    core: TARGET_ARCH_VARIANT supports x86-slm
    BZ: 120393
    
    TARGET_ARCH_VARIANT now supports x86-slm. Need to search for x86-atom
    and x86-slm in Android.mk
    
    Change-Id: I788f0984b7db62f178de70f8e91d9b158ef6578d
    Category: device enablement
    Domain: PLATFORM-Platform
    Origin: internal
    Upstream-Candidate: no
    Signed-off-by: Chetan Rao <chetan.rao@intel.com>

--- libcutils/Android.mk
+++ libcutils/Android.mk
@@ -119,16 +119,16 @@ LOCAL_SRC_FILES := $(commonSources) \
 ifeq ($(TARGET_ARCH),arm)
 LOCAL_SRC_FILES += arch-arm/memset32.S
 else  # !arm
-ifeq ($(TARGET_ARCH_VARIANT),x86-atom)
+ifneq (, $(findstring x86-, $(TARGET_ARCH_VARIANT)))
 LOCAL_CFLAGS += -DHAVE_MEMSET16 -DHAVE_MEMSET32
 LOCAL_SRC_FILES += arch-x86/android_memset16.S arch-x86/android_memset32.S memory.c
-else # !x86-atom
+else # !x86-atom and !x86-slm
 ifeq ($(TARGET_ARCH),mips)
 LOCAL_SRC_FILES += arch-mips/android_memset.c
 else # !mips
 LOCAL_SRC_FILES += memory.c
 endif # !mips
-endif # !x86-atom
+endif # !x86-atom and !x86-slm
 endif # !arm
 
 LOCAL_C_INCLUDES := $(libcutils_c_includes) $(KERNEL_HEADERS)
