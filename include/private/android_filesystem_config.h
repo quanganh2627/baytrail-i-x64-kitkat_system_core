0a3684ef6ae0b9eb27156e7b9229d6614d9d2c65
Tomas Winkler <tomas.winkler@intel.com>
2013-08-05 10:44:50 +0300

    Create intel_sec aid for accessing Intel security devices
    Issue: APDEV-504
    
    Category: device enablement
    Domain: AOSP.Security
    Origin: internal
    Upstream-Candidate: no, Intel specific group
    
    Change-Id: I7995a9dc27ba2fe5ff6d1cd118e09f447b8282f9

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -91,6 +91,7 @@
 #define AID_NET_BW_STATS  3006  /* read bandwidth statistics */
 #define AID_NET_BW_ACCT   3007  /* change bandwidth statistics accounting */
 #define AID_NET_BT_STACK  3008  /* bluetooth: access config files */
+#define AID_INTEL_SEC     3701  /* intel security device */
 
 #define AID_MISC          9998  /* access to misc storage */
 #define AID_NOBODY        9999
@@ -164,6 +165,7 @@ static const struct android_id_info android_ids[] = {
     { "net_bw_stats",  AID_NET_BW_STATS, },
     { "net_bw_acct",   AID_NET_BW_ACCT, },
     { "net_bt_stack",  AID_NET_BT_STACK, },
+    { "intel_sec",     AID_INTEL_SEC, },
 
     { "misc",          AID_MISC, },
     { "nobody",        AID_NOBODY, },
