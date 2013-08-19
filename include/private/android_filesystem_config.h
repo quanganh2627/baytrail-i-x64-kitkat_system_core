954fa8eb1b9c4bd4a20a214de619c4eb9b5fad50
Fei Yang <fei.yang@intel.com>
2013-08-19 15:05:25 -0700

    [PORT FROM MAIN] Rebase 41706: Establish security group on android for SEP
    BZ: 48207
    
    DO NOT UPSTREAM
    
    Bug: 48207
    
    Change-Id: Ide6d222548714ebfbb31e9870d0c7f9288bf5cd1
    Category: device enablement
    Domain: Security-Chaabi_Driver_Discretix (SEP)
    Origin: internal
    Upstream-Candidate: no
    Signed-off-by: Mark Allyn <mark.a.allyn@intel.com>
    Reviewed-on: http://android.intel.com:8080/126263
    Reviewed-by: Chen, HuaqiangX <huaqiangx.chen@intel.com>
    Reviewed-by: Yao, Yong <yong.yao@intel.com>
    Reviewed-by: Shuster, Norvin <norvin.shuster@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -91,6 +91,7 @@
 #define AID_NET_BW_STATS  3006  /* read bandwidth statistics */
 #define AID_NET_BW_ACCT   3007  /* change bandwidth statistics accounting */
 #define AID_NET_BT_STACK  3008  /* bluetooth: access config files */
+#define AID_SEP_GROUP     3700  /* This is for the security processor */
 
 #define AID_MISC          9998  /* access to misc storage */
 #define AID_NOBODY        9999
@@ -166,6 +167,7 @@ static const struct android_id_info android_ids[] = {
     { "net_bt_stack",  AID_NET_BT_STACK, },
 
     { "misc",          AID_MISC, },
+    { "security",  AID_SEP_GROUP },
     { "nobody",        AID_NOBODY, },
 };
 
