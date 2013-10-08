9d1fc0597db7e916f911b3546fdc9fa8f6c35583
Jayachandran B <jayachandran.b@intel.com>
2013-10-08 10:34:24 +0530

    [REVERTME] Add "/data" path to request_firmware search path
    BZ: 140844
    
    VTSV service updates the VTSV files which the SST driver
    then loads using the request_firmware() infrastructure.
    VTSV service can not access any of the standard firware paths.
    Hence it places the files in "/data". Therefore add this path
    to the request_firmware search path.
    
    This patch is a temporary one to enable the VTSV service to update
    VTSV files dynamically. Ideally the files should not be placed
    in "/data" folder. They should be placed in a VTSV specific directory
    with appropriate access control. This patch will be reverted
    once such a scheme is in place.
    
    Change-Id: Id78c3e9d842ea76f0eece80d5c946d5e5ad99242
    Category: feature differentiation
    Domain: AUDIO_MEDIA-VTSV
    Origin: internal
    Upstream-Candidate: no, proprietary
    Signed-off-by: Jayachandran B <jayachandran.b@intel.com>
    Signed-off-by: Kishore Nandury <venkata.k.nandury@intel.com>

--- init/devices.c
+++ init/devices.c
@@ -59,6 +59,7 @@
 #define FIRMWARE_DIR1   "/etc/firmware"
 #define FIRMWARE_DIR2   "/vendor/firmware"
 #define FIRMWARE_DIR3   "/firmware/image"
+#define FIRMWARE_DIR4   "/data"
 #define MODULES_BLKLST  "/ueventd.modules.blacklist"
 #define CRDA_BIN_PATH   "/system/bin/crda"
 #define PLATFORM_STR    "platform"
@@ -1188,7 +1189,7 @@ static int is_booting(void)
 
 static void process_firmware_event(struct uevent *uevent)
 {
-    char *root, *loading, *data, *file1 = NULL, *file2 = NULL, *file3 = NULL;
+    char *root, *loading, *data, *file1 = NULL, *file2 = NULL, *file3 = NULL, *file4 = NULL;
     int l, loading_fd, data_fd, fw_fd;
     int booting = is_booting();
 
@@ -1219,6 +1220,10 @@ static void process_firmware_event(struct uevent *uevent)
     if (l == -1)
         goto data_free_out;
 
+    l = asprintf(&file4, FIRMWARE_DIR4"/%s", uevent->firmware);
+    if (l == -1)
+        goto data_free_out;
+
     loading_fd = open(loading, O_WRONLY);
     if(loading_fd < 0)
         goto file_free_out;
@@ -1234,17 +1239,20 @@ try_loading_again:
         if (fw_fd < 0) {
             fw_fd = open(file3, O_RDONLY);
             if (fw_fd < 0) {
-                if (booting) {
-                        /* If we're not fully booted, we may be missing
-                         * filesystems needed for firmware, wait and retry.
-                         */
-                    usleep(100000);
-                    booting = is_booting();
-                    goto try_loading_again;
+                fw_fd = open(file4, O_RDONLY);
+                if (fw_fd < 0) {
+                    if (booting) {
+                            /* If we're not fully booted, we may be missing
+                             * filesystems needed for firmware, wait and retry.
+                             */
+                        usleep(100000);
+                        booting = is_booting();
+                        goto try_loading_again;
+                    }
+                    INFO("firmware: could not open '%s' %d\n", uevent->firmware, errno);
+                    write(loading_fd, "-1", 2);
+                    goto data_close_out;
                 }
-                INFO("firmware: could not open '%s' %d\n", uevent->firmware, errno);
-                write(loading_fd, "-1", 2);
-                goto data_close_out;
             }
         }
     }
@@ -1263,6 +1271,7 @@ file_free_out:
     free(file1);
     free(file2);
     free(file3);
+    free(file4);
 data_free_out:
     free(data);
 loading_free_out:
