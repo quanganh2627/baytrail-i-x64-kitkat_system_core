e8e2cef542c740ecd3465357771218d1dead50c2
Guilhem IMBERTON <guilhem.imberton@intel.com>
2014-02-06 18:37:09 +0100

    [SDK] Aligning ARM includes
    BZ: 143553
    
    Change-Id: Ie5f3ab499e6ef1ab358c9ec584467754e0509242
    Category: aosp improvement
    Domain: AOSP-Others
    Origin: internal
    Upstream-Candidate: yes
    Signed-off-by: Guilhem IMBERTON <guilhem.imberton@intel.com>

--- libpixelflinger/tests/codegen/Android.mk
+++ libpixelflinger/tests/codegen/Android.mk
@@ -1,23 +1,20 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
+LOCAL_MODULE:= test-opengl-codegen
+LOCAL_MODULE_TAGS := tests
 
-LOCAL_SRC_FILES:= \
-	codegen.cpp
+LOCAL_SRC_FILES := \
+    codegen.cpp
 
 LOCAL_SHARED_LIBRARIES := \
-	libcutils \
-	libpixelflinger
+    libcutils \
+    libpixelflinger
 
 LOCAL_C_INCLUDES := \
-	system/core/libpixelflinger
+    system/core/libpixelflinger
 
 ifeq ($(TARGET_ARCH),x86)
-LOCAL_C_INCLUDES += \
-	$(TOP)/vendor/intel/hardware/apache-harmony
+LOCAL_C_INCLUDES += $(TARGET_OUT_HEADERS)/libenc
 endif
-
-LOCAL_MODULE:= test-opengl-codegen
-
-LOCAL_MODULE_TAGS := tests
 
 include $(BUILD_EXECUTABLE)
