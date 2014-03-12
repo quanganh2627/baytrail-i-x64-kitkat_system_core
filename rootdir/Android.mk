3becebe2f96fa592087ac9b4c82d69d9731b14e0
Olivier Fourdan <olivier.fourdan@intel.com>
2014-03-12 10:32:51 +0100

    Revert "Allow customization of init.environ.rc"
    BZ: 178592
    
    This reverts commit d6aded4e3f6860f213e22211b0cf848131a7717b.
    
    Change-Id: I4de0da6f5eca13005bedf24c4cb5b2b072a104e1
    Category: aosp improvement
    Domain: AOSP-Framework-CoreService
    Origin: internal
    Upstream-Candidate: no, not-aosp
    Signed-off-by: Olivier Fourdan <olivier.fourdan@intel.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -30,11 +30,7 @@ LOCAL_POST_INSTALL_CMD := mkdir -p $(addprefix $(TARGET_ROOT_OUT)/, \
 
 include $(BUILD_SYSTEM)/base_rules.mk
 
-# Give a chance to use an alternate init.environ.rc.in if
-# TARGET_ENVIRON_RC_IN is set.
-TARGET_ENVIRON_RC_IN ?= $(LOCAL_PATH)/init.environ.rc.in
-
-$(LOCAL_BUILT_MODULE): $(TARGET_ENVIRON_RC_IN)
+$(LOCAL_BUILT_MODULE): $(LOCAL_PATH)/init.environ.rc.in
 	@echo "Generate: $< -> $@"
 	@mkdir -p $(dir $@)
 	$(hide) sed -e 's?%BOOTCLASSPATH%?$(PRODUCT_BOOTCLASSPATH)?g' $< >$@
