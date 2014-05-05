f476b33aa8148db10d2aeb5620c4f76986bc9625
Thierry Escande <thierry.escande@linux.intel.com>
2014-05-05 19:01:10 +0200

    Add AID_HAL as permitted uid for property service
    This adds AID_HAL in the permissions white list for setting property
    service. It is allowed for any kind of properties, i.e. no prefix is
    specified for property names.
    
    Issue: GMIN-615
    Change-Id: Ibf5b6051899ba23f8e7f6585aa1119a02af63e36
    Signed-off-by: Thierry Escande <thierry.escande@linux.intel.com>
    Reviewed-on: https://android.intel.com/187627
    Reviewed-by: Ortiz, Samuel <samuel.ortiz@intel.com>
    Tested-by: Ortiz, Samuel <samuel.ortiz@intel.com>

--- init/property_service.c
+++ init/property_service.c
@@ -94,6 +94,7 @@ struct {
     { "persist.security.", AID_SYSTEM,   0 },
     { "persist.service.bdroid.", AID_BLUETOOTH,   0 },
     { "selinux."         , AID_SYSTEM,   0 },
+    { "",                  AID_HAL,      0 },
     { NULL, 0, 0 }
 };
 
