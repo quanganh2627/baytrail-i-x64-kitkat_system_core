dcb6ec72421b129f5b2f7dc5a7079614e0ba9861
Huajun Li <huajun.li@intel.com>
2015-01-12 15:43:17 +0800

    skip slow wake alarm
    BZ: 567
    
    When on battery, slow wake alarm intends to watch for drained battery,
    actually, SW FG sets wake alarm too, so don't need this one here to
    avoid system wakes up freqently.
    This patch enhances commit 75fdbe2e59ee853757d72b81f07166cb539dfde1
    
    Change-Id: Icd15c562ab353b33d0773d20c05c6d848f836388
    Signed-off-by: Huajun Li <huajun.li@intel.com>

--- healthd/healthd.cpp
+++ healthd/healthd.cpp
@@ -36,7 +36,7 @@ using namespace android;
 
 // Periodic chores intervals in seconds
 #define DEFAULT_PERIODIC_CHORES_INTERVAL_FAST (60 * 1)
-#define DEFAULT_PERIODIC_CHORES_INTERVAL_SLOW (60 * 60 * 2)
+#define DEFAULT_PERIODIC_CHORES_INTERVAL_SLOW (-1)
 
 static struct healthd_config healthd_config = {
     .periodic_chores_interval_fast = DEFAULT_PERIODIC_CHORES_INTERVAL_FAST,
